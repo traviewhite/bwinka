b62cc53f2f4903005a4443b58934308903e43c57
Travis White <traviewhite@gmail.com>
2020-11-04 16:47:31 -0600

    netlify setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withImages = require('next-images')
 module.exports = withImages();
 
 module.exports = {
+  target: "serverless",
   distDir: 'build',
   devIndicators: {
     autoPrerender: false,
